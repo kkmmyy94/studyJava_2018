a99bba808170725d7fe19e64fef1cdef7b501b31
kkmmyy94 <kkmmyy94@naver.com>
2018-10-06 15:49:47 +0900

    Battle Mode Logic (どうでもいいから一応動ける様にすればいいのかな)

--- studyJavaMonsterBattle/src/com/kkmmyy94/www/mode/BattleModeImpl.java
+++ studyJavaMonsterBattle/src/com/kkmmyy94/www/mode/BattleModeImpl.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 public class BattleModeImpl implements Mode {
 
+	Object userData;
+	Object MonsterData;
+	
 	@Override
 	public boolean init() {
 		// TODO Auto-generated method stub
@@ -17,6 +20,58 @@ public class BattleModeImpl implements Mode {
 		List returnValue = new LinkedList();
 		returnValue.add("0");
 		return returnValue;
+	}
+	
+	private void battleInit() {
+		//Set userData
+		//set MonsterData
+	}
+	
+	private void battlePvE() {
+		
+		attack();
+		
+		//switch attack result
+		
+		//get Monster
+		
+		//print result
+		
+		//End BattleMode
+	}
+	
+	private void getMonsterWhenUserWin() {
+		//Are you want throw MonsterBall?
+		
+		//User throw
+		
+		//check  User Can get monster really 
+		
+		//if get Monster and return 
+		
+		//else return result value
+		
+	}
+	
+	private void printResult() {
+		//readd Rersult
+		
+		//print result
+		
+		
+	}
+	
+	private void attack() {
+		//LoopBegin
+		
+		//user want Attack?
+		//Attack
+		//Is enermy Lived?
+		//Attack
+		
+		//Loop End
+		
+		//return ResultValue
 	}
 
 }
